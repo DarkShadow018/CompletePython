adf873efd635bad7129dd55c9e562c57fbcca7c0
Yash Gupta <50226252+DarkShadow018@users.noreply.github.com>
2021-12-04 02:13:13 +0530

    Initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,17 @@
+name1 = "Yash"
+age1 =  20
+
+name2 = "Shreyas"
+age2 = 20.0
+
+is_adult = True
+
+print("Hello", name1 + " and his age is :", age1)
+print("Hello", name2 + " and his age is :", age2)
+
+#Custom Input Program
+
+name = input("Enter Your Name : ")
+age = input("Enter Your Age : ")
+
+print("Hello", name + " your age is", age + " and you are a genius")
